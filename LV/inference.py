a3a0436ae8a3f52ce97f7bf045fb30459c82cab6
nadavbennun1 <nadavbennun1@mail.tau.ac.il>
2024-08-21 14:19:30 +0300

    Foldering

---
+++ LV/inference.py
@@ -0,0 +1,11 @@
+import sbibm
+
+task = sbibm.  # See sbibm.get_available_tasks() for all tasks
+prior = task.get_prior()
+simulator = task.get_simulator()
+observation = task.get_observation(num_observation=1)  # 10 per task
+
+# These objects can then be used for custom inference algorithms, e.g.
+# we might want to generate simulations by sampling from prior:
+thetas = prior(num_samples=10_000)
+xs = simulator(thetas)
