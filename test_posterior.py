30f938035c093ea5338c5770c688d04617534c09
nadavbennun1 <nadavbennun1@mail.tau.ac.il>
2025-01-14 16:01:20 +0200

    full evaluation of hierarchical WF

--- test_posterior.py
+++ test_posterior.py
@@ -55,7 +55,7 @@ def evaluate_cp(posterior, thetas, n_samples):
         samples = cp.sample(n_samples)
         params = torch.tensor(thetas[i,:], dtype=torch.float32)
         accus[i] = samples.mean(0)-params
-        # covs[i] = (cp.log_prob(samples) > cp.log_prob(params)).sum()/n_samples
+        covs[i] = (cp.log_prob(samples) > cp.log_prob(params)).sum()/n_samples
         print(i)
         if i%10 == 9:
           print(f'{round(100*(i+1)/len(thetas),2)}%')
