2e21b394aab278456e6318baea836bca3b2f0438
nadavbennun1 <nadavbennun1@mail.tau.ac.il>
2025-10-28 08:11:28 +0200

    test fixes and glu test data

--- test_posterior.py
+++ test_posterior.py
@@ -52,7 +52,7 @@ conf_levels = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95]
 
 # Load the test thetas
 thetas = torch.load(thetas_dir)
-
+X = torch.load(x_dir)
 
 def coverage_old(posterior, samples, conf_levels, theta):
     covs = torch.empty(len(conf_levels), len(theta))
